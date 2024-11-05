bfac4fdbb3fa2978323d9a626bc4339d4cce46e9
Benedicta Ikemefune <emmanuelashinze612@gmail.com>
2024-11-05 21:52:10 +0100

    corrected few errors

--- Backend/skatech/settings.py
+++ Backend/skatech/settings.py
@@ -39,7 +39,6 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'articles',    # Custom app for articles
     'media',       # Custom app for media content
-    'users',       # Custom app for user management
 ]
 
 
